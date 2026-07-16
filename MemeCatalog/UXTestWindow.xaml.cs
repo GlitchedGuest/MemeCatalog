3274719f5c8efbae0d9fbaf4388d4c0052cc3e8e
GlitchedGuest <andrzejparuch2003@gmail.com>
2026-07-16 18:09:37 +0200

    +Nowe dodawanie tagów do memów +Możliwość tworzenia nowych tagów +Możliwość usuwania memów +Możliwość modyfikacji nazwy memów +Możliwość zmiany tagów w memach +Wyczyszczono pobieżnie kod

--- MemeCatalog/UXTestWindow.xaml.cs
+++ MemeCatalog/UXTestWindow.xaml.cs
@@ -20,128 +20,260 @@ using System.Windows.Threading;
 
 namespace MemeCatalog
 {
-    /// <summary>
-    /// Logika interakcji dla klasy UXTestWindow.xaml
-    /// </summary>
     public partial class UXTestWindow : Window
     {
+        //Variables for db operations
         private readonly MemeContext _context = new MemeContext();
-        private List<Tag> tags = new List<Tag>();
-        private WrapPanel wrappanel = new WrapPanel();
-        Meme currentMeme = null;
+        private HashSet<String> _tagsSet = new HashSet<String>();
+
+        //Wrappanel for buttons
+        private WrapPanel _wrapPanelBrowseMemes = new WrapPanel();
+        private WrapPanel _wrapPanelBrowseTags = new WrapPanel();
+
+        //Variables for Creating/editing Memes
+        private Meme _meme = null;
+        private Button _buttonReference = null;
+
+
+        //App initialize
 
         public UXTestWindow()
         {
             InitializeComponent();
-
         }
-
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             _context.Database.EnsureCreated();
-            //LVMemesList.ItemsSource = _context.Memes.ToList();
-            //TestUC._button.Click += BrowseMemesButton_Click;
-            CreateButtons();
+            LoadMemesButtons();
+            LoadTagsButtons();
         }
 
-        private void BrowseButton_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.ShowDialog();
+        //Loading buttons
 
-            MP_AddMeme.LoadMedia(ofd.FileName);
-            Tb_Path.Text = ofd.FileName;
-            Tb_Name.Text = ofd.SafeFileName;
-        }
-        private void AddButton_Click(object sender, RoutedEventArgs e)
+        private Button CreateTagButton(Tag tag)
         {
-            Meme newMeme = new Meme
+            Button _button = new Button();
+
+            _button.Margin = new Thickness(15, 10, 10, 10);
+            _button.VerticalAlignment = VerticalAlignment.Top;
+            _button.HorizontalAlignment = HorizontalAlignment.Left;
+            _button.Height = 50;
+            _button.Width = 100;
+            _button.Background = Brushes.White;
+            _button.Content = tag.Name;
+            _button.Click += TagButtonClick;
+
+            return _button;
+        }
+        private void LoadTagsButtons()
+        {
+            _context.Tags.Load();
+
+            _wrapPanelBrowseTags.Orientation = Orientation.Horizontal;
+            _wrapPanelBrowseTags.HorizontalAlignment = HorizontalAlignment.Left;
+            _wrapPanelBrowseTags.VerticalAlignment = VerticalAlignment.Top;
+
+            var _tagList = _context.Tags.ToList();
+
+            foreach (var i in _tagList)
             {
-                Name = Tb_Name.Text,
-                Path = Tb_Path.Text
+                _wrapPanelBrowseTags.Children.Add(CreateTagButton(i));
+            }
+            SV_BrowseTags.Content = _wrapPanelBrowseTags;
+            
+        }
+        private Button CreateMemeButton(Meme meme)
+        {
+            Button _button = new Button();
+            UCMemeButton _UCSButton = new UCMemeButton();
+
+            _button.Margin = new Thickness(15, 10, 10, 10);
+            _button.VerticalAlignment = VerticalAlignment.Top;
+            _button.HorizontalAlignment = HorizontalAlignment.Left;
+            _button.Background = Brushes.Transparent;
+            _button.Height = 120;
+            _button.Width = 100;
+            _UCSButton.SetMeme(meme);
+            _button.Content = _UCSButton;
+            _button.Click += InspectMemeButtonClick;
+
+            return _button;
+        }
+        private void LoadMemesButtons()
+        {
+
+            _wrapPanelBrowseMemes.Orientation = Orientation.Horizontal;
+            _wrapPanelBrowseMemes.HorizontalAlignment = HorizontalAlignment.Left;
+            _wrapPanelBrowseMemes.VerticalAlignment = VerticalAlignment.Top;
+
+            var _memeList = _context.Memes.ToList();
+
+            foreach (var i in _memeList)
+            {
+                _wrapPanelBrowseMemes.Children.Add(CreateMemeButton(i));
+            }
+
+            SV_BrowseMemes.Content = _wrapPanelBrowseMemes;
+        }
+
+        //Upload Meme Segement
+
+        private void UploadButtonClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog _ofd = new OpenFileDialog();
+            _ofd.ShowDialog();
+
+            MP_UploadMeme.LoadMedia(_ofd.FileName);
+            TBlock_Upload_Path.Text = _ofd.FileName;
+            TBox_Upload_Name.Text = _ofd.SafeFileName;
+            Btn_AddTags.IsEnabled = true;
+            Btn_AddToDatabase.IsEnabled = true;
+
+            _meme = new Meme()
+            {
+                Name = TBox_Upload_Name.Text,
+                Path = TBlock_Upload_Path.Text
             };
-            tags.ForEach(x => newMeme.Tags.Add(_context.Tags.Where(y => y.TagId == x.TagId).ToList()[0]));
-            _context.Memes.Add(newMeme);
+        }
+        private void AddToDatabaseButtonClick(object sender, RoutedEventArgs e)
+        {
+            var _tags = _context.Tags.Where(x => _tagsSet.Contains(x.Name)).ToList();
+            _tags.ForEach(x => _meme.Tags.Add(x));
+
+            _context.Memes.Add(_meme);
             _context.SaveChanges();
-            //mediaElement.Source = null;
-            Tb_Path.Text = "";
-            Tb_Name.Text = "";
-           // LVMemesList.ItemsSource = _context.Memes.Select(x => x.Name).ToList();
+
+            _tagsSet.Clear();
+            MP_UploadMeme.DeloadMedia();
+            TBlock_Upload_Path.Text = "";
+            TBox_Upload_Name.Text = "";
+            Btn_AddTags.IsEnabled = false;
+            Btn_AddToDatabase.IsEnabled = false;
+
+            _wrapPanelBrowseMemes.Children.Add(CreateMemeButton(_meme));
+
+            _meme = null;
         }
-        private void AddTagButton_Click(object sender, RoutedEventArgs e)
+        private void AddTagButtonClick(object sender, RoutedEventArgs e)
         {
-            UXTagAdd tagWindow = new UXTagAdd();
-
-            tagWindow.ShowDialog();
-            tags = tagWindow.tags;
-
-        }
-
-        private void BrowseMemesButton_Click(object sender, RoutedEventArgs e)
-        {
-            var browse = TBBrowse.Text;
-            var memeList = _context.Memes.Select(x => x.Name).Where(x => !x.Contains(browse)).ToList();
-            foreach(var i in wrappanel.Children)
+            Grid_Tags.Visibility = Visibility.Visible;
+            foreach (Button i in _wrapPanelBrowseTags.Children)
             {
-                if (memeList.Contains((i as UCMemeButton)._button.Text))
+                if (_tagsSet.Contains(i.Content))
+                    i.Background = Brushes.Gray;
+            }
+        }
+
+        //Browse Memes
+
+        private void UpdateMemeButtonClick(object sender, RoutedEventArgs e)
+        {
+            _meme.Name = TBox_Inspect_FileName.Text;
+            (_buttonReference.Content as UCMemeButton)._button.Text = TBox_Inspect_FileName.Text;
+            _context.SaveChanges();
+        }
+        private void UpdateTagsButtonClick(object sender, RoutedEventArgs e)
+        {
+            foreach (Button i in _wrapPanelBrowseTags.Children)
+            {
+                if (_tagsSet.Contains(i.Content))
+                    i.Background = Brushes.Gray;
+            }
+            Grid_Tags.Visibility = Visibility.Visible;
+        }
+        private void DeleteMemeButtonClick(object sender, RoutedEventArgs e)
+        {
+            _context.Memes.Remove(_meme);
+
+            _wrapPanelBrowseMemes.Children.Remove(_buttonReference);
+            MP_InspectMeme.DeloadMedia();
+            _buttonReference = null;
+            _meme = null;
+            Grid_InspectMeme.Visibility = Visibility.Collapsed;
+            Grid_BrowseMemes.Visibility = Visibility.Visible;
+
+            _context.SaveChanges();
+        }
+        private void InspectMemeBackButtonClick(object sender, RoutedEventArgs e)
+        {
+            _meme.Tags.Clear();
+            var _tags = _context.Tags.Where(x => _tagsSet.Contains(x.Name)).ToList();
+            _tags.ForEach(x => _meme.Tags.Add(x));
+            _context.SaveChanges();
+            _tagsSet.Clear();
+            _meme = null;
+            Grid_InspectMeme.Visibility = Visibility.Collapsed;
+            Grid_BrowseMemes.Visibility = Visibility.Visible;
+            MP_InspectMeme.DeloadMedia();
+        }
+        private void ExplorerButtonClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("explorer", $"/select, {TBlock_Inspect_FilePath.Text}");
+        }
+        private void SearchMemesButtonClick(object sender, RoutedEventArgs e)
+        {
+            var _browse = TBox_SearchBar.Text;
+            var _memeList = _context.Memes.Select(x => x.Name).Where(x => !x.Contains(_browse)).ToList();
+            foreach (var i in _wrapPanelBrowseMemes.Children)
+            {
+                if (_memeList.Contains((i as UCMemeButton)._button.Text))
                     (i as UCMemeButton).Visibility = Visibility.Collapsed;
                 else
                     (i as UCMemeButton).Visibility = Visibility.Visible;
             }
         }
-
-        private void CreateButtons()
+        private void InspectMemeButtonClick(object sender, RoutedEventArgs e)
         {
-            int offsetLeft = -100;
-            int offsetTop = -100;
-            
-            wrappanel.Orientation = Orientation.Horizontal;
-            wrappanel.HorizontalAlignment = HorizontalAlignment.Left;
-            wrappanel.VerticalAlignment = VerticalAlignment.Top;
+            Grid_BrowseMemes.Visibility = Visibility.Collapsed;
+            Grid_InspectMeme.Visibility = Visibility.Visible;
+            _buttonReference = sender as Button;
+            _meme = (_buttonReference.Content as UCMemeButton).meme;
+            _tagsSet = _meme.Tags.Select(x => x.Name).ToHashSet();
+            MP_InspectMeme.LoadMedia(_meme.Path);
+            TBox_Inspect_FileName.Text = ((sender as Button).Content as UCMemeButton).meme.Name;
+            TBlock_Inspect_FilePath.Text = ((sender as Button).Content as UCMemeButton).meme.Path;
+        }
 
-            var memeList = _context.Memes.ToList();
+        //Tag
 
-            foreach(var i in memeList)
+        private void TagButtonClick(object sender, RoutedEventArgs e)
+        {
+            if ((sender as Button).Background == Brushes.Gray)
             {
-                Button _b = new Button();
-                UCMemeButton b = new UCMemeButton();
-
-                _b.Margin = new Thickness(15, 10, 10, 10);
-                _b.VerticalAlignment = VerticalAlignment.Top;
-                _b.HorizontalAlignment = HorizontalAlignment.Left;
-                _b.Background = Brushes.Transparent;
-                _b.Height = 120;
-                _b.Width = 100;
-                b.SetMeme(i);
-                _b.Content = b;
-                _b.Click += ShowMemeButton_Click;
-                wrappanel.Children.Add(_b);
+                (sender as Button).Background = Brushes.White;
+                _tagsSet.Remove((sender as Button).Content.ToString());
             }
+            else
+            {
+                (sender as Button).Background = Brushes.Gray;
 
-            ScrollViewer.Content = wrappanel;
+                _tagsSet.Add((sender as Button).Content.ToString());
+            }
         }
+        private void AddTagBackButtonClick(object sender, RoutedEventArgs e)
+        {
+            Grid_Tags.Visibility = Visibility.Collapsed;
+            foreach (Button i in _wrapPanelBrowseTags.Children)
+            {
+                if (i.Background == Brushes.Gray)
+                    i.Background = Brushes.White;
+            }
+        }
+        private void AddNewTagButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (TBox_AddTag.Text == "")
+                return;
+            Tag _newTag = new Tag()
+            {
+                Name = TBox_AddTag.Text
+            };
+            _context.Tags.Add(_newTag);
+            _context.SaveChanges();
 
-        private void ShowMemeButton_Click(object sender, RoutedEventArgs e)
-        {
-            BrowseMemeGrid.Visibility = Visibility.Collapsed;
-            ShoweMemeGrid.Visibility = Visibility.Visible;
-            currentMeme = ((sender as Button).Content as UCMemeButton).meme;
-            MP_ShowMeme.LoadMedia(currentMeme.Path);
-            TB_Show_FileName.Text = ((sender as Button).Content as UCMemeButton).meme.Name;
-            TB_Show_FilePath.Text = ((sender as Button).Content as UCMemeButton).meme.Path;
-        }
+            _wrapPanelBrowseTags.Children.Add(CreateTagButton(_newTag));
 
-        
-        private void ShowMemeBackButton_Click(object sender, RoutedEventArgs e)
-        {
-            currentMeme = null;
-            ShoweMemeGrid.Visibility = Visibility.Collapsed;
-            BrowseMemeGrid.Visibility = Visibility.Visible;
-            MP_ShowMeme.DeloadMedia();
-        }
-        private void ExplorerButton_Click(object sender, RoutedEventArgs e)
-        {
-            Process.Start("explorer", $"/select, {TB_Show_FilePath.Text}");
+            TBox_AddTag.Text = "";
+
         }
     }
 }
